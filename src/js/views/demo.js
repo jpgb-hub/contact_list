87773cfd6ed46c8c0fb05d1beac2b4924bf28016
Juan Pablo Guerrero <57576332+jpgb-hub@users.noreply.github.com>
2023-08-29 03:53:18 +0000

    listo

--- src/js/views/demo.js
+++ src/js/views/demo.js
@@ -1,43 +1,84 @@
-import React, { useState, useEffect, useContext } from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import "../../styles/demo.css"
 
-import { Context } from "../store/appContext";
 
-import "../../styles/demo.css";
+function Demo() {
+  const [formData, setFormData] = useState({
+    name: '',
+    address: '',
+    phone: '',
+    email: '',
+  });
+  const navigate = useNavigate();
 
-export const Demo = () => {
-	const { store, actions } = useContext(Context);
+  // Carga los contactos del localStorage al montar el componente y
+  // actualiza el estado con esos datos
+  const getContacts = () => {
+    const contacts = JSON.parse(localStorage.getItem('contacts')) || [];
+    return contacts;
+  };
 
-	return (
-		<div className="container">
-			<ul className="list-group">
-				{store.demo.map((item, index) => {
-					return (
-						<li
-							key={index}
-							className="list-group-item d-flex justify-content-between"
-							style={{ background: item.background }}>
-							<Link to={"/single/" + index}>
-								<span>Link to: {item.title}</span>
-							</Link>
-							{// Conditional render example
-							// Check to see if the background is orange, if so, display the message
-							item.background === "orange" ? (
-								<p style={{ color: item.initial }}>
-									Check store/flux.js scroll to the actions to see the code
-								</p>
-							) : null}
-							<button className="btn btn-success" onClick={() => actions.changeColor(index, "orange")}>
-								Change Color
-							</button>
-						</li>
-					);
-				})}
-			</ul>
-			<br />
-			<Link to="/">
-				<button className="btn btn-primary">Back home</button>
-			</Link>
-		</div>
-	);
-};
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const contacts = getContacts();
+    contacts.push(formData);  // Añadimos el nuevo contacto
+
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+    setFormData({
+      name: '',
+      address: '',
+      phone: '',
+      email: '',
+    });
+
+    navigate('/'); // Volvemos al home después de agregar el contacto
+  };
+
+  return (
+    <div className="App">
+      <h1>Ingresa un contacto</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            Nombre:
+            <input type="text" name="name" value={formData.name} onChange={handleChange} required />
+          </label>
+        </div>
+        <div>
+          <label>
+            Dirección:
+            <input type="text" name="address" value={formData.address} onChange={handleChange} required />
+          </label>
+        </div>
+        <div>
+          <label>
+            Teléfono:
+            <input type="tel" name="phone" value={formData.phone} onChange={handleChange} required />
+          </label>
+        </div>
+        <div>
+          <label>
+            Email:
+            <input type="email" name="email" value={formData.email} onChange={handleChange} required />
+          </label>
+        </div>
+        <div>
+          <button type="submit">Guardar Contacto</button>
+        </div>
+      </form>
+      <button onClick={() => navigate('/')}>Volver al Home</button>
+    </div>
+  );
+}
+
+export default Demo;
+
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,60 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
+import React, { useState, useEffect } from 'react';
+import { MdMail, MdPhone, MdEdit, MdDelete, MdAdd } from 'react-icons/md';
+import { useNavigate } from 'react-router-dom';
+import "../../styles/home.css"
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+function Home() {
+    const [contacts, setContacts] = useState([]);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const storedContacts = JSON.parse(localStorage.getItem('contacts'));
+        if (storedContacts) setContacts(storedContacts);
+    }, []);
+
+    const handleAddClick = () => {
+        navigate('/demo');
+    };
+
+    const handleEdit = (index) => {
+        navigate(`/demo/${index}`);
+    };
+
+    const handleDelete = (index) => {
+        if (window.confirm("¿Estás seguro de querer eliminar este contacto?")) {
+            const updatedContacts = [...contacts];
+            updatedContacts.splice(index, 1);
+            setContacts(updatedContacts);
+            localStorage.setItem('contacts', JSON.stringify(updatedContacts));
+        }
+    };
+
+    return (
+        <div>
+            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <h2>Mis Contactos</h2>
+                <button onClick={handleAddClick} style={{ backgroundColor: '#4CAF50', color: 'white', padding: '8px 16px', border: 'none', borderRadius: '4px', cursor: 'pointer' }}>
+                    <MdAdd /> Agregar contacto
+                </button>
+            </div>
+
+            {contacts.map((contact, index) => (
+                <div className="contact-card" key={index}>
+                    <img src="path/to/default/image.jpg" alt="contact" className="contact-image" />
+                    <div className="contact-info">
+                        <strong>{contact.name}</strong><br />
+                        <MdPhone /> {contact.phone}<br />
+                        <MdMail /> {contact.email}<br />
+                        <span>{contact.address}</span><br />
+                    </div>
+                    <div className="contact-actions">
+                        <button onClick={() => handleEdit(index)}><MdEdit /></button>
+                        <button onClick={() => handleDelete(index)}><MdDelete /></button>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default Home;
